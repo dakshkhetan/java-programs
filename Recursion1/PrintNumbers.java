28759bf04db2cbee65b59ff7ab5fa34cebef7e1c
Daksh Khetan <2012daksh@gmail.com>
2020-02-08 16:28:25 +0530

    added program

---
+++ Recursion1/PrintNumbers.java
@@ -0,0 +1,52 @@
+package Recursion1;
+
+/* **********
+
+	Print Numbers
+	
+	Given the code to print number from 1 to n in increasing order recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
+	
+	Input Format :
+	Integer n
+	
+	Output Format :
+	Numbers from 1 to n (separated by space)
+	
+	Constraints :
+	1 <= n <= 10000
+	
+	Sample Input :
+	 6
+	
+	Sample Output :
+	1 2 3 4 5 6
+
+************/
+
+/* **********
+
+import java.util.Scanner;
+
+public class Runner {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		Solution.print(n);
+	}
+	
+}
+
+************/
+
+public class PrintNumbers {
+
+	public static void print(int n){
+      
+		if(n == 0) {
+			return;
+		}
+		print(n - 1);
+		System.out.print(n + " ");
+	}
+
+}
