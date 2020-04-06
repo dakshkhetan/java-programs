aec6e192659a108bdbd30f54d8b2b88e34df44ec
Daksh Khetan <2012daksh@gmail.com>
2020-04-06 23:07:14 +0530

    added program

--- StringsAnd2DArrays/AllSubstringsOfAString.java
+++ StringsAnd2DArrays/AllSubstringsOfAString.java
@@ -6,13 +6,17 @@ package StringsAnd2DArrays;
 	
 	Given a String S of length n, print all its substrings.
 	Substring of a String S is a part of S (of any length from 1 to n), which contains all consecutive characters from S.
+	
 	Input Format :
 	String S
+	
 	Output Format :
-	 All Substrings of S, one in each line.
+	All Substrings of S, one in each line.
 	Note : The order in which you print substrings doesn't matter.
+	
 	Sample Input 1 :
 	xyz
+	
 	Sample Output 1 :
 	x
 	xy
