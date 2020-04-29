c72ff4af536bce230c421b438de0c721861635be
Daksh Khetan <2012daksh@gmail.com>
2020-04-29 23:27:19 +0530

    added program

--- StringsAnd2DArrays/WavePrint.java
+++ StringsAnd2DArrays/WavePrint.java
@@ -54,12 +54,11 @@ public class WavePrint {
 	public static void wavePrint(int arr[][]){
 		
 		for(int j = 0; j < arr[0].length; j++) {
-			if(j %2 == 0) {
+			if(j % 2 == 0) {
 				for(int i = 0; i < arr.length; i++) {
 					System.out.print(arr[i][j] + " ");
 				}
-			}
-			else {
+			} else {
 				for(int i = arr.length - 1; i >= 0; i--) {
 					System.out.print(arr[i][j] + " ");
 				}
